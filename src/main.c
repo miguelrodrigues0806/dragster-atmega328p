011f34f582f92356c9dbd5540dc6d7934e9692d9
miguelrodrigues0806 <miguelrodrigues0806@gmail.com>
2026-09-18 16:12:18 +0100

    build: config ambiente AVR bare-metal e arq. de pastas

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <avr/io.h>
+
+int main(void) {
+    // Configuração inicial de hardware e interrupções
+    
+    while (1) {
+        // Ciclo de controlo de trajetória e velocidade
+    }
+    return 0;
+}
